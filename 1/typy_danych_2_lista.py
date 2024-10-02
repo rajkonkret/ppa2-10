cfe4ee0c9ab1d05486bc7290363ed21b53a86259
rjaniak <rajkonkret660@gmail.com>
2024-10-02 15:21:08 +0200

    [1.15] update typy_danych_2_lista.py

--- 1/typy_danych_2_lista.py
+++ 1/typy_danych_2_lista.py
@@ -34,3 +34,44 @@ print(lista[-1])  # Ania, ostatni element
 print(lista[-2])
 print(lista[-3])  # Marek
 
+# wyświetlanie fragmentu listy (slicowanie)
+print(lista[0:3])  # ['Radek', 'Zenek', 'Tomek'], tylko indeksy 012
+print(lista[:3])  # ['Radek', 'Zenek', 'Tomek']
+print(lista[2:])  # ['Tomek', 'Marek', 'Magda', 'Ania'], indeksy 2,3,4,5
+print(lista[2:5])  # ['Tomek', 'Marek', 'Magda']
+print(lista[2:6])  # ['Tomek', 'Marek', 'Magda', 'Ania']
+print(lista[10:20])  # []
+print(lista[:])  # ['Radek', 'Zenek', 'Tomek', 'Marek', 'Magda', 'Ania']
+
+print(lista[-2:0])  # -> [4:0] # []
+print(lista[0:-2])  # ['Radek', 'Zenek', 'Tomek', 'Marek']
+print(lista[2:3])  # ['Tomek']
+print(lista[0:5:2])  # ['Radek', 'Tomek', 'Magda']
+# [start:stop:krok]
+
+# nadpisanie elementu w liście
+lista[3] = "Mikołaj"
+print(lista)  # ['Radek', 'Zenek', 'Tomek', 'Mikołaj', 'Magda', 'Ania']
+
+# insert() - wstawienie elementu we wskazany indeks
+lista.insert(1, "Karol")
+print(lista)  # ['Radek', 'Karol', 'Zenek', 'Tomek', 'Mikołaj', 'Magda', 'Ania']
+
+# spraawdzenie indeksu dla danego elementu
+print(lista.index("Mikołaj"))  # indeks numer 4
+lista.append("Mikołaj")
+lista.append("Mikołaj")
+print(lista)  # ['Radek', 'Karol', 'Zenek', 'Tomek', 'Mikołaj', 'Magda', 'Ania', 'Mikołaj']
+print(lista.index("Mikołaj"))  # index zwraca pierwszy element, 4
+# ['Radek', 'Karol', 'Zenek', 'Tomek', 'Mikołaj', 'Magda', 'Ania', 'Mikołaj', 'Mikołaj']
+
+# usunięcie elementu z listy, usunie pierwsze wystąpienie elementu
+lista.remove("Mikołaj")
+print(lista)  # ['Radek', 'Karol', 'Zenek', 'Tomek', 'Magda', 'Ania', 'Mikołaj', 'Mikołaj']
+
+# usunięcie po indeksie, pokaże co usunął
+print(lista.pop(5))  # Ania
+print(lista)  # ['Radek', 'Karol', 'Zenek', 'Tomek', 'Magda', 'Mikołaj', 'Mikołaj']
+print(lista.pop(-2))  # Mikołaj
+print(lista)  # ['Radek', 'Karol', 'Zenek', 'Tomek', 'Magda', 'Mikołaj']
+print(lista.pop())  # Mikołaj, usunie ostatni element z listy
