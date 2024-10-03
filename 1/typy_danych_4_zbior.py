cba7837cd35ce4486019016b060ed5840b93d7bd
rjaniak <rajkonkret660@gmail.com>
2024-10-03 10:17:26 +0200

    [2.4] update typy_danych_4_zbior.py

--- 1/typy_danych_4_zbior.py
+++ 1/typy_danych_4_zbior.py
@@ -29,3 +29,27 @@ print(zbior)  # {66, 777, 11, 44, 18, 22, 24}
 zbior_copy = zbior.copy()
 print(zbior)
 print(id(zbior), id(zbior_copy))  # 2788340719776 2788340715296
+
+zbior_2 = {667, 11, 44, 18, 52, 62, 999, 999, 12.34}
+print(zbior_2)  # {999, 11, 44, 12.34, 18, 52, 667, 62}
+print(type(zbior_2))  # <class 'set'>
+
+# suma zbiorów
+print(zbior | zbior_2)  # {66, 999, 777, 11, 44, 12.34, 18, 52, 22, 24, 667, 62}
+print(zbior.union(zbior_2))  # {66, 999, 777, 11, 44, 12.34, 18, 52, 22, 24, 667, 62}
+
+# częśc wspólna zbiorów
+print(zbior & zbior_2)  # {18, 11, 44}
+print(zbior.intersection(zbior_2))  # {18, 11, 44}
+
+# róznica
+print(zbior - zbior_2)  # {24, 777, 66, 22}
+print(zbior.difference(zbior_2))  # {24, 777, 66, 22}
+print(zbior_2.difference(zbior))  # {999, 12.34, 52, 667, 62}
+
+lista = list(zbior)
+print(lista)  # [66, 777, 11, 44, 18, 22, 24]
+print(type(lista))  # <class 'list'>
+
+# sprawdzenie czy element istnieje w kolekcji
+print(999 in zbior_2)  # True
