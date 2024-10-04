1337b5d03221f6783d97b636f8c4054ff20a9ae7
rjaniak <rajkonkret660@gmail.com>
2024-10-04 09:33:08 +0200

    [3.1] update fun1

--- 2/fun1.py
+++ 2/fun1.py
@@ -13,6 +13,32 @@ def dodaj():  # funkcja bez argumentów
     print(a + b)
 
 
+def dodaj2(a, b):  # dwa obowiązkowe argumenty
+    print(a + b)  # argumenty lokalne
+
+
+def odejmij(a, b, c=0):  # c - argument z wartością domyślną
+    print(a - b - c)
+
+
 # uruchomienie funkcji
 # nazwa_funkcji i nawiasy ()
 dodaj()  # 14
+# dodaj2()# TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
+
+# przekazywanie przez pozycję, argumenty pozycyjne
+dodaj2(5, 9)  # 14
+odejmij(1, 2)  # c=0, -1
+odejmij(1, 2, 3)  # c=3, -4
+
+# przekazania argumentów po nazwie,
+odejmij(c=9, a=8, b=10)  # -11
+odejmij(b=9, a=7)  # -2
+
+# mieszane
+odejmij(1, c=10, b=34)  # -43
+# odejmij(a=10, 2, 3)  # SyntaxError: positional argument follows keyword argument
+
+print(dodaj())  # None
+
+# print(dodaj() + dodaj2())  # TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
